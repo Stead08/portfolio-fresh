86e5c37a16a8808011ae3ef5b2fd43f71af3de7b
Stead08 <inomatama08@gmail.com>
2023-04-24 18:11:25 +0900

    add deps.ts

--- lib/MicroCMSClient.ts
+++ lib/MicroCMSClient.ts
@@ -1,6 +1,7 @@
 import { createClient } from "microcms";
+import { env } from "./deps.ts";
 
 export const microcmsClient = createClient({
     serviceDomain: "vzh782w7u9",
-    apiKey: `${Deno.env.get("X-MICROCMS-API-KEY")}`,
+    apiKey: `${env.get("X-MICROCMS-API-KEY")}`,
 });
