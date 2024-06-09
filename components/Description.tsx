95f108eeafbfadd9ce97edb1d34f3a074cfac6eb
Stead08 <62312581+Stead08@users.noreply.github.com.>
2024-06-09 10:43:58 +0900

    update: description

--- components/Description.tsx
+++ components/Description.tsx
@@ -4,15 +4,11 @@ const Description = () => {
       <h3 className="text-3xl font-bold uppercase text-gray-600 lg:text-center">
         Description
       </h3>
-      <div className="bg-gray-50 shadow-lg rounded-lg p-2 text-center h-fullf">
-        <h4 className="text-xl font-semibold">こんにちは</h4>
-        <p className="text-gray-700 mt-2">
-          Stead08といいます。 24卒で新卒エンジニアとして働く予定です。
-        </p>
-        <p className="text-gray-700">
-          フロントエンドよりもサーバーサイドの方に興味があります。
-        </p>
-        <p className="text-gray-700">
+      <div className="flex justify-center flex-col bg-gray-50 shadow-lg rounded-lg p-2 h-fullf">
+        <h4 className="text-xl mx-auto w-full md:w-auto font-semibold">こんにちは</h4>
+        <p className="block mx-auto w-full md:w-auto text-gray-700 mt-2 mb-2 ">
+          Stead08といいます。 24卒でフロントエンドエンジニアとして働いてます。<br/>
+          フロントエンドもサーバーサイドの方に興味があります。<br/>
           好き嫌いなくどちらもできるように現在勉強しています。
         </p>
       </div>
